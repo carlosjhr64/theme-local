099663aec652ffefa12e897f8b056ff374397ee0
carlosjhr64 <carlosjhr64@gmail.com>
2016-01-25 08:26:11 -0800

    added git_ahead color coded git status

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,6 +1,27 @@
-function _is_git_dirty
-  # Modified from Krisleech theme...
-  echo (command git status --porcelain --ignore-submodules=dirty ^/dev/null)
+function _git_color
+  set -l ahead    "↑"
+  set -l behind   "↓"
+  set -l diverged "⥄ "
+  set -l dirty    "⨯"
+  set -l none     "◦"
+  set -l head (git_ahead $ahead $behind $diverged $none)
+
+  set -l color "green"
+  set -l git (command git status --porcelain ^/dev/null | egrep -o '^(..)')
+  if test -n "$git"
+    if test $git[1] = "??"
+      set color "magenta"
+    else
+      set color "red"
+      set head $dirty
+    end
+  else
+    if test "$head" != "$none"
+      set color "yellow"
+    end
+  end
+
+  echo -n -s (set_color $color) $head
 end
 
 function fish_right_prompt
@@ -8,10 +29,7 @@ function fish_right_prompt
   set -l normal (set_color normal)
   set -l color $normal
   if test -e .git
-    set color (set_color green)
-    if [ (_is_git_dirty) ]
-      set color (set_color red)
-    end
+    set color (_git_color)
   end
   set -l d (date '+%T')
   echo -n -s $color $d $normal
